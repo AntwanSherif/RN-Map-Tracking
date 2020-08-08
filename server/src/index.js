395a5b77e2c720ffd41668ff5b4edf4f0b38a322
AntwanSherif <antwansherif@gmail.com>
2020-08-08 22:30:19 +0200

    :construction: Create User model

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,5 @@
+require('./models/User');
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
 const router = express.Router();
 
 router.post('/signup', (req,  res) => {
