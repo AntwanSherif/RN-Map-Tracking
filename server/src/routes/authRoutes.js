fba2bfc3bcc1cefdbec580a8a916c64115374bf6
AntwanSherif <antwansherif@gmail.com>
2020-08-08 22:48:38 +0200

    :sparkles: Create jwt on signup

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
 const router = express.Router();
@@ -11,7 +12,8 @@ router.post('/signup', async (req,  res) => {
     const user = new User({ email, password });
     await user.save();
     
-    res.send({ message: 'successful signup!' });
+    const token = jwt.sign({ userId:  user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
   } catch (error) {
     res.status(422).send({ message: error.message });
   }
