9bb49f59655602246a4880cbd1681f44ce99eb42
AntwanSherif <antwansherif@gmail.com>
2020-08-08 22:07:44 +0200

    :card_file_box: Connect to remote mongodb instance

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@itclinic-u1gma.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connection.on('connected', () =>  {
+  console.log('connected to mongo instance');
+});
+mongoose.connection.on('error', error =>  {
+  console.log('Error connecting to mongo', error);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi there!')
