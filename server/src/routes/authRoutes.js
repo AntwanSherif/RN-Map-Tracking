b953324d0d88ec29cdead946aef834b0c6a77696
AntwanSherif <antwansherif@gmail.com>
2020-08-10 01:07:45 +0200

    :sparkles: User signin & passowrd hashing

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -19,4 +19,27 @@ router.post('/signup', async (req,  res) => {
   }
 });
 
+router.post('/signin', async (req,  res) => {
+  const { email, password } = req.body;
+
+  if(!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
+
+  const user = await User.findOne({ email });
+  
+  if(!user) {
+    return res.status(422).send({ error: 'Invalid email or password' });
+  }
+  
+  try {
+    await user.comparePassword(password);
+    
+    const token = jwt.sign({ userId:  user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
+  } catch (error) {
+    res.status(422).send({ error: 'Invalid email or password' });
+  }
+});
+
 module.exports = router;
