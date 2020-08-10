cd48128dc144218ef017cf18525d28f50f039ffb
AntwanSherif <antwansherif@gmail.com>
2020-08-10 22:25:44 +0200

    :art: Add prettier rules

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  useTabs: false,
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: true,
+  trailingComma: "none",
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  arrowParens: "avoid",
+  noSemi: true,
+  rcVerbose: true,
+};
